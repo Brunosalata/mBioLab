99fb965836e836b96c6f90d12f591a3b8b2d7358
Biopdi <127441676+Biopdi@users.noreply.github.com>
2023-04-25 17:14:49 -0300

    Chart Working
    Essay, Single-Report, Multi-Dashboard
    method ChartClear

--- src/main/java/br/com/biopdi/mbiolabv2/controller/SceneController/DashboardSceneController.java
+++ src/main/java/br/com/biopdi/mbiolabv2/controller/SceneController/DashboardSceneController.java
@@ -1,17 +1,16 @@
 package br.com.biopdi.mbiolabv2.controller.SceneController;
 
 import br.com.biopdi.mbiolabv2.controller.repository.dao.*;
+import br.com.biopdi.mbiolabv2.model.bean.Essay;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.ResourceBundle;
 
 public class DashboardSceneController implements Initializable {
@@ -24,19 +23,13 @@ public class DashboardSceneController implements Initializable {
 
     @FXML
     private Label lbCurrentData;
-
     @FXML
-    private CategoryAxis xChartSingle;
+    private Button btnLed, btnChartClear;
     @FXML
-    private NumberAxis yChartSingle,xChartMulti, yChartMulti;// xChartSingle,
+    private TextField txtLed;
     @FXML
-    private LineChart<?,?> xyChartSingle, xyChartMulti;
-    private XYChart.Series series;
-
-    Date systemDate = new Date();
-    SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    SimpleDateFormat brasilianDate = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-    String currentDate = brasilianDate.format(systemDate);
+    private LineChart<Number,Number> chartMultiLine;
+    private XYChart.Series seriesMulti;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -45,59 +38,34 @@ public class DashboardSceneController implements Initializable {
     }
 
     @FXML
-    private void comparativeChartAnalisis(){
-        //defining the axes
-        xChartMulti.setLabel("Deslocamento");
-        yChartMulti.setLabel("Força");
+    private void addEssayChart(int pk) {
+        seriesMulti = new XYChart.Series();
+        //populating the series with data
 
-        xyChartMulti.setTitle("Compartivo Força x Posição");
+        Essay essay = essayDAO.findById(pk);
+        seriesMulti.setName(essay.getEssayIdentification());
+        // 1;1,2;2,3;3,4;4,5;5,6;6,7;7,8;8,9;9,10;10
 
-        XYChart.Series series1 = new XYChart.Series();
-        series1.setName("Ensaio 1");
+        String strArraySplit[] = essay.getEssayChart().split(",");
+        for(String str : strArraySplit){
+            String dot[] = str.split(";");
+            for(int i = 0; i<dot.length;i+=2){
+                System.out.println(dot[i] + " " + dot[i+1]);
+                seriesMulti.getData().add(new XYChart.Data(Double.parseDouble(dot[i]),Double.parseDouble(dot[i+1])));
+            }
+        }
+        chartMultiLine.getData().add(seriesMulti);
 
-        series1.getData().add(new XYChart.Data(1, 23));
-        series1.getData().add(new XYChart.Data(2, 14));
-        series1.getData().add(new XYChart.Data(3, 15));
-        series1.getData().add(new XYChart.Data(4, 24));
-        series1.getData().add(new XYChart.Data(5, 34));
-        series1.getData().add(new XYChart.Data(6, 36));
-        series1.getData().add(new XYChart.Data(7, 22));
-        series1.getData().add(new XYChart.Data(8, 45));
-        series1.getData().add(new XYChart.Data(9, 43));
-        series1.getData().add(new XYChart.Data(10, 17));
-        series1.getData().add(new XYChart.Data(11, 29));
-        series1.getData().add(new XYChart.Data(12, 25));
-
-        XYChart.Series series2 = new XYChart.Series();
-        series2.setName("Ensaio 2");
-        series2.getData().add(new XYChart.Data(1, 33));
-        series2.getData().add(new XYChart.Data(2, 34));
-        series2.getData().add(new XYChart.Data(3, 25));
-        series2.getData().add(new XYChart.Data(4, 44));
-        series2.getData().add(new XYChart.Data(5, 39));
-        series2.getData().add(new XYChart.Data(6, 16));
-        series2.getData().add(new XYChart.Data(7, 55));
-        series2.getData().add(new XYChart.Data(8, 54));
-        series2.getData().add(new XYChart.Data(9, 48));
-        series2.getData().add(new XYChart.Data(10, 27));
-        series2.getData().add(new XYChart.Data(11, 37));
-        series2.getData().add(new XYChart.Data(12, 29));
-
-        XYChart.Series series3 = new XYChart.Series();
-        series3.setName("Ensaio 3");
-        series3.getData().add(new XYChart.Data(1, 44));
-        series3.getData().add(new XYChart.Data(2, 35));
-        series3.getData().add(new XYChart.Data(3, 36));
-        series3.getData().add(new XYChart.Data(4, 33));
-        series3.getData().add(new XYChart.Data(5, 31));
-        series3.getData().add(new XYChart.Data(6, 26));
-        series3.getData().add(new XYChart.Data(7, 22));
-        series3.getData().add(new XYChart.Data(8, 25));
-        series3.getData().add(new XYChart.Data(9, 43));
-        series3.getData().add(new XYChart.Data(10, 44));
-        series3.getData().add(new XYChart.Data(11, 45));
-        series3.getData().add(new XYChart.Data(12, 44));
-
-        xyChartMulti.getData().addAll(series1, series2, series3);
     }
+
+    @FXML
+    private void led(){
+        addEssayChart(Integer.parseInt(txtLed.getText()));
+    }
+
+    @FXML
+    private void chartClear(){
+        chartMultiLine.getData().clear();
+    }
+
 }
