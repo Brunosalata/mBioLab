46c45e656e9d7e7fd6c7ed1bbe5b8a0e6d4a37b4
Bruno Salata <brunoslima@gmail.com>
2023-06-20 17:44:13 -0300

    SplashScene inside loginStage working

--- src/main/java/br/com/biopdi/mbiolabv2/controller/SceneController/SplashSceneController.java
+++ src/main/java/br/com/biopdi/mbiolabv2/controller/SceneController/SplashSceneController.java
@@ -14,12 +14,22 @@ package br.com.biopdi.mbiolabv2.controller.SceneController;
  *  limitations under the License.
  */
 
+import br.com.biopdi.mbiolabv2.mBioLabv2Application;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Bruno Salata Lima - 20/06/2023
@@ -34,6 +44,40 @@ public class SplashSceneController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+//        apSplash.setClip(new Circle(233,233,233));
+        new SplashScreen().start();
     }
+
+    class SplashScreen extends Thread{
+        public void run(){
+            try {
+                Thread.sleep(2000);
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        Parent login = null;
+                        try {
+                            login = FXMLLoader.load(mBioLabv2Application.class.getResource("loginScene.fxml"));
+                        } catch (IOException e) {
+                            Logger.getLogger(SplashSceneController.class.getName()).log(Level.SEVERE, null, e);
+                        }
+
+                        Stage stage = new Stage();
+                        stage.setTitle("mBioLab");
+                        stage.getIcons().add(new Image(getClass().getResourceAsStream("img/iconBiopdi.png")));
+                        stage.setResizable(false);  // Impede redimensionamento da janela
+                        stage.setScene(new Scene(login));
+                        stage.show();
+
+                        apSplash.getScene().getWindow().hide();
+                    }
+                });
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
 }
